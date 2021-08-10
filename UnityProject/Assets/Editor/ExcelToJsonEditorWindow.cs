7d90839bf5ea39f4db2461e791cc7ea07c8d6868
Dabin <dorothy9795@gmail.com>
2021-08-10 23:45:39 +0900

    설정 윈도우 추가

--- UnityProject/Assets/Editor/ExcelToJsonEditorWindow.cs
+++ UnityProject/Assets/Editor/ExcelToJsonEditorWindow.cs
@@ -5,17 +5,47 @@ using UnityEngine;
 
 public class ExcelToJsonEditorWindow : EditorWindow
 {
-    private static readonly string SettingWindowTitle = "ExcelToJsonSettings";
+    private const string SettingWindowTitle = "ExcelToJsonSettings";
+    private const string EdtiorSettingDir = "Assets/ExcelToJson";
+    private const string EditorSettingResDir = "Assets/ExcelToJson/Resources";
+    private const string EditorSettingFile = "Assets/ExcelToJson/Resources/ExcelToJsonSettings.asset";
+
+    public static ExcelToJsonSettings settings;
 
     [MenuItem("DataBase/ExcelToJsonSettings")]
 
     public static void ShowWindow()
     {
         GetWindow(typeof(ExcelToJsonEditorWindow)).titleContent.text = SettingWindowTitle;
+
+        if (!AssetDatabase.IsValidFolder(EditorSettingResDir))
+            AssetDatabase.CreateFolder(EdtiorSettingDir, "Resources");
+
+        LoadSettings();
     }
 
-    void OnGUI()
+    private void OnGUI()
     {
+        GUILayout.Label("Excel Path");
 
+        settings.ExcelPath = EditorGUILayout.TextField("Text Field", settings.ExcelPath);
+    }
+
+    private void OnDestroy()
+    {
+        SaveSettings();
+    }
+
+    public static void SaveSettings()
+    {
+        var instance = CreateInstance<ExcelToJsonSettings>();
+        AssetDatabase.CreateAsset(instance, EditorSettingFile);
+        AssetDatabase.SaveAssets();
+    }
+
+    public static void LoadSettings()
+    {
+        if (AssetDatabase.LoadAssetAtPath(EditorSettingFile, typeof(ExcelToJsonSettings)) == null)
+            SaveSettings();
     }
 }
---
+++ UnityProject/Assets/Editor/ExcelToJsonSettings.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class ExcelToJsonSettings : ScriptableObject
+{
+    public string ExcelPath = "Assets/Editor";
+}
