5ec0832894fe14e10761c730210dd5514c777e46
Dabin <dorothy9795@gmail.com>
2021-08-10 22:28:43 +0900

    init

---
+++ UnityProject/Assets/Editor/ExcelToJsonEditorWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ExcelToJsonEditorWindow : EditorWindow
+{
+    private static readonly string SettingWindowTitle = "ExcelToJsonSettings";
+
+    [MenuItem("DataBase/ExcelToJsonSettings")]
+
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(ExcelToJsonEditorWindow)).titleContent.text = SettingWindowTitle;
+    }
+
+    void OnGUI()
+    {
+
+    }
+}
